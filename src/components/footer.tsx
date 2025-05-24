b5246966d35ba15e04f9d9b73477a9b076316d18
franck-ru <franckyeptie@gmail.com>
2025-05-24 02:38:35 +0100

    notificafion frontend

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,20 +9,20 @@ const Footer: React.FC = () => {
           {/* Colonne 1 : Contact */}
           <div>
             <h3 className="text-lg font-semibold mb-3">Contact</h3>
-            <p className="text-gray-600">email@example.com</p>
-            <p className="text-gray-600">+123 456 789</p>
+            <p className="text-gray-600">franckyeptie@gmail.com</p>
+            <p className="text-gray-600">+237-699-619-034</p>
           </div>
 
           {/* Colonne 2 : Adresse */}
           <div>
             <h3 className="text-lg font-semibold mb-3">Adresse</h3>
-            <p className="text-gray-600">123 Rue Example</p>
-            <p className="text-gray-600">Paris, France</p>
+            <p className="text-gray-600">nkomkana-rue principal </p>
+            <p className="text-gray-600">yaounde, cameroun</p>
           </div>
 
           {/* Colonne 3 : Horaires */}
           <div>
-            <h3 className="text-lg font-semibold mb-3">Horaires</h3>
+            <h3 className="text-lg font-semibold mb-3">Horaires de passage</h3>
             <p className="text-gray-600">Lun-Ven : 9h-18h</p>
             <p className="text-gray-600">Sam : 10h-14h</p>
           </div>
